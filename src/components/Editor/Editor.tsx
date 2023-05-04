701b19a8e8088522f8a9f6c253e37d0b41362cde
brygmos <daniel-best@yandex.ru>
2023-05-04 13:22:12 +0300

    feat:  basic editor setup

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
-import cl from './MyEditor.module.css';
-
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
-import 'codemirror/theme/3024-day.css';
-import 'codemirror/mode/javascript/javascript';
-import CodeMirror from 'codemirror';
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { useState } from 'react';
+import cl from './Editor.module.css';
 
 export const Editor = () => {
-  const editorRef = useRef(null);
-  const [responce, setResponce] = useState('');
+  const [response, setResponse] = useState('');
   const [query, setQuery] = useState(
     'query AllCharacters {\n' +
       '  characters {\n' +
@@ -19,24 +15,6 @@ export const Editor = () => {
       '  }\n' +
       '}'
   );
-  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    // setValue(event.target.value);
-  };
-
-  useEffect(() => {
-    const editor = CodeMirror.fromTextArea(editorRef.current, {
-      mode: 'javascript',
-      theme: '3024-day',
-      lineNumbers: true,
-      value: 'qwerty',
-      lint: true,
-      lineWrapping: true,
-      spellcheck: true,
-    });
-    editor.on('change', (instance) => {
-      setQuery(instance.getValue());
-    });
-  }, []);
 
   const extractQueryName = (query: string) => {
     const text = query;
@@ -61,27 +39,48 @@ export const Editor = () => {
         return response.json();
       })
       .then((data) => {
-        setResponce(data);
+        setResponse(data);
       });
   };
 
-  const handleClick = (event: React.MouseEvent) => {
+  const handleClick = () => {
     fetchUserData();
   };
+
   return (
     <>
       <div className={cl.container}>
         <div className={cl.container__left}>
           <div className={cl.editor}>
-            <label>Enter your query:</label>
-            <textarea ref={editorRef} value={query} />
+            <label style={{ color: 'white' }}>Enter your query:</label>
+            <CodeMirror
+              value={query}
+              height="200px"
+              theme={'light'}
+              autoFocus={true}
+              extensions={[javascript({ jsx: true })]}
+              onChange={(value) => {
+                setQuery(value);
+              }}
+            />
             <button onClick={handleClick}>SEND</button>
           </div>
         </div>
         <div className={cl.container__right}>
-          <div className={cl.responce}>
-            {/*{responce.data && <pre>{JSON.stringify(responce, null,2)}</pre>}*/}
-            <pre>{responce.data && JSON.stringify(responce, null, 2)}</pre>
+          <div className={cl.response}>
+            <CodeMirror
+              basicSetup={{ lineNumbers: false }}
+              value={JSON.stringify(response, null, 2)}
+              height="400px"
+              readOnly={true}
+              style={{ overflowY: 'scroll' }}
+              placeholder="here will be api response"
+              theme={'light'}
+              extensions={[javascript({ jsx: true })]}
+              // onChange={(value, viewUpdate) => {
+              //   setQuery(value);
+              // }}
+            />
           </div>
         </div>
       </div>
@@ -89,4 +88,4 @@ export const Editor = () => {
   );
 };
 
-export default MyEditor;
+export default Editor;
