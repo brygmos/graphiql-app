c724366c2f60008ad917b0e2418f596369ed51ec
Sasha <hlupik69@gmail.com>
2023-05-06 18:48:08 +0300

    feat: add sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
 import EditorPage from './pages/EditorPage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
   return (
     <div className="App">
       <EditorPage />
+      <SignInPage />
+      <SignUpPage />
     </div>
   );
 }
