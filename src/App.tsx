63fc4170e2c895277c32bc9f9478e7d8cd055949
brygmos <daniel-best@yandex.ru>
2023-05-10 11:20:01 +0300

    fix: add editor to route

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import { Routes, Route } from "react-router-dom";
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
 import { Welcome } from './pages/Welcome/Welcome';
-import { Layout } from "./components/Layout/Layouut";
+import { Layout } from './components/Layout/Layouut';
 import EditorPage from './pages/EditorPage';
 import ObjectExplorer from './components/test';
 
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={<Layout />} >
+      <Route path="/" element={<Layout />}>
         <Route index element={<Welcome />} />
-        <Route element={<EditorPage />} />
+        <Route path="/editor" element={<EditorPage />} />
       </Route>
-      
-     </Routes>
-  )
+    </Routes>
+  );
 }
 
-export default App
+export default App;
