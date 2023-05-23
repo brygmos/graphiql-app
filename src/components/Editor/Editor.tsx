7bb9834965d2e2a40c9ce6d2e738d82e18bb30a3
brygmos <daniel-best@yandex.ru>
2023-05-23 12:40:08 +0300

    fix: edit introsp. query

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -40,8 +40,92 @@ const Editor = () => {
   const prefersDarkMode =
     window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
+  const schemaQuery2 = `query IntrospectionQuery {
+      __schema {
+        queryType { name }
+        types {
+          ...FullType
+        }
+        directives {
+          name
+          locations
+          args {
+            ...InputValue
+          }
+        }
+      }
+    }
+
+    fragment FullType on __Type {
+      kind
+      name
+      fields {
+        name
+        args {
+          ...InputValue
+        }
+        type {
+          ...TypeRef
+        }
+        isDeprecated
+        deprecationReason
+      }
+      inputFields {
+        ...InputValue
+      }
+      interfaces {
+        ...TypeRef
+      }
+      enumValues {
+        name
+        isDeprecated
+        deprecationReason
+      }
+      possibleTypes {
+        ...TypeRef
+      }
+    }
+
+    fragment InputValue on __InputValue {
+      name
+      type { ...TypeRef }
+      defaultValue
+    }
+
+    fragment TypeRef on __Type {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+                ofType {
+                  kind
+                  name
+                  ofType {
+                    kind
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`;
   const schemaQuery =
-    'query IntrospectionQuery {__schema {queryType { name } types { name kind description fields { name description args { name description type { name kind ofType { name kind } } } } } }}';
+    'query IntrospectionQuery {__schema {queryType { name } types { name kind description fields { name description type { name } args { name description type { name kind ofType { name kind } } } } } }}';
 
   useEffect(() => {
     prefersDarkMode ? setTheme(ThemeType.dark) : setTheme(ThemeType.light);
